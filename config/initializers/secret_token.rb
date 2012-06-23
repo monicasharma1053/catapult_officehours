6ec6b80d1ff668c03ff32fd8cd3ccd3d16040396
Monica Sharma <monicasharma1053@gmail.com>
2012-06-23 18:17:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatapultOfficehours::Application.config.secret_token = '4c9b6b887a6fae2ceaa2f50277830769e326c0420eb134b5ba38ac9f3d0ac15b137fd0ed454a0f61c0ed8d902e2229f39112d1345e97e8069363fbea32c0bb32'

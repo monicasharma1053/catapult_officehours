fa1d81816673780d449bfd9814168459e75947d6
Monica Sharma <monicasharma1053@gmail.com>
2012-07-18 19:01:37 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 CatapultOfficehours::Application.routes.draw do
 
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
   root to: 'static_pages#home'
+  
   match '/help',  to: 'static_pages#help'
   match '/signup', to: 'users#new'
+  
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
 
   # The priority is based upon order of creation:
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,10 +22,13 @@ describe "UserPages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
+      
+      describe "after saving the user" do
+        it { should have_link('Sign out') }
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+      end      
     end
   end
 
